164932bee25fe2cfe9707e59d03704044ab60e31
Marco Spina <spina.marco@outlook.it>
2019-11-14 17:01:21 +0100

    added start scene

--- Negroni/StartScene.swift
+++ Negroni/StartScene.swift
@@ -1,6 +1,36 @@
 import SpriteKit
-import GameplayKit
-import AVFoundation
-import CoreAudio
 
-//terst
+
+import SpriteKit
+
+class StartScene: SKScene {
+    
+    var playButton:SKSpriteNode?
+    var gameScene:SKScene!
+        
+    override func didMove(to view: SKView) {
+        playButton = self.childNode(withName: "startButton") as? SKSpriteNode
+    
+    
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let pos = touch.location(in: self)
+            let node = self.atPoint(pos)
+            
+            if node == playButton {
+                let transition = SKTransition.fade(withDuration: 1)
+                gameScene = SKScene(fileNamed: "GameScene")
+                gameScene.scaleMode = .aspectFit
+                self.view?.presentScene(gameScene, transition: transition)
+                
+            }
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        }
+    }
+
+
